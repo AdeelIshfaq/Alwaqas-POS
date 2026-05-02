afb80aed23fee51370831827bdd0e8c1d16b5fe6
Adeel Ishfaq <adeel@sharplogician.com>
2026-05-03 00:47:54 +0500

    Update for Vercel

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,14 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     * - auth routes (login, callback, etc.)
+     */
     '/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$|auth/).*)',
   ],
 };
